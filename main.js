2c27f5ac9a24398b596bfe0b251bb88cfaade3db
darmond157 <bardiarezaeikalantari@gmail.com>
2024-09-07 13:40:48 +0330

    init

---
+++ main.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const fastify = require("fastify")({ logger: true });
+
+const counter = new client.Counter({
+	name: "api_requests_count",
+	help: "counting requests to the api",
+});
+
+fastify.get("/ping", (req, res) => {
+	counter.inc();
+	res.send("pong");
+});
+
+fastify.get("/metrics", async (req, res) => {
+	res.header("Content-Type", client.register.contentType);
+	res.send(await client.register.metrics());
+});
+
+fastify.listen({ port: process.env.PORT, host: process.env.HOST });
